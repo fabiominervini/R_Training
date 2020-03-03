d8ca86237f6c6445e0c8fa38e414bcc2ce12c98a
fabiominervini <fabio.minervini@gmail.com>
2020-03-02 21:31:03 -0300

    Aula 1

--- Teste Aula.R
+++ Teste Aula.R
@@ -3,11 +3,11 @@ setwd("C:\\Fabio\\Curso R")
 # vendo o que tem dentro do diret?rio
 dir()
 
-sqrt(2+2)
-(2+2)^(1/2)
+sqrt(2+2) #raiz quadrada
+(2+2)^(1/2)# ^ = potência
 2*2
 
-exp(2)
+exp(2) #exponencial
 12+(16*5)
 sqrt(12+16)*5
 # arredondando o resultado
@@ -15,7 +15,7 @@ round(sqrt(12+16)*5, digits = 2)
 # atibuindo a express?o a um objeto
 a <- round(sqrt(12+16)*5, digits = 2)
 
-#criando vetores
+#criando vetores - sempre coloca "c" antes dos atributos
 (v <- c(4,6,1,2.2))
 3+v
 3*v
@@ -30,27 +30,27 @@ a
 
 c(6,7,8,9,10) -> b
 
-d = c(2,4,6,8,10)
+d = c(2,4,6,8,10) # o sinal de = substitui o <-
 
 e <- c(1,2,3,4,5)
 e
 (f <- c(1,2,3,4,5))
 
-rm(a,b,d,e,f)
-rm(list=ls())
+rm(a,d,e,f) #remove = rm
+rm(list=ls())# limpa todo o environment
 
 (v <- c(4,6,1,2.2))
 
-mean(v) #m?dia do vetor
-min(v)  # menor n?mero do vetor
-max(v)  # maior n?mero do vetor
+mean(v) #média do vetor
+min(v)  # menor número do vetor
+max(v)  # maior número do vetor
 length(v) # comprimento do vetor
 
 (vf <- c(4,6,1,NA))
 mean (vf) #deu ruim pq tem dado faltante (NA)
 mean(vf,na.rm=TRUE) # o comando para retirar o dado faltante do comando
 
-#criando sequ?ncias
+#criando sequências
 1:10
 1:1000
 1000:1050
@@ -59,14 +59,13 @@ seq(from=1,to=100,by=1)
 seq(from=1,to=100,by=5)
 seq(from=0,to=100,by=5)
 
-#combinando uma sequ?ncia com um vetor
+#combinando uma sequência com um vetor
 (z=c(v,seq(5,85,5)))
 
 (w <- c(1,-1,2,-2))
 w
-str(w)
-Class(w)
-class(w)
+str(w) # apresenta as informações do objeto
+class(w) # class mostra de que tipo é o elemento
 
 rm(z)
 z <- 1+w
